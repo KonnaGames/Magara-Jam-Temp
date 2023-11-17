a99d27a17241ea35f3a1ab34fe8ccc4438cfa46d
Flecherus <malikaras7634@gmail.com>
2023-11-17 18:14:40 +0300

    Assets

---
+++ Assets/_Project/Scripts/BossMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMove : MonoBehaviour
+{
+    [SerializeField] private Transform[] transformPoints;
+    [SerializeField] private float speed;
+
+    private int currentPoint;
+
+    private void Start()
+    {
+        currentPoint = 0;
+    }
+    void Update()
+    {
+        if (transform.position != transformPoints[currentPoint].position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, transformPoints[currentPoint].position, speed * Time.deltaTime);
+        }
+        else
+        {
+            currentPoint = (currentPoint + 1) % transformPoints.Length;
+        }
+    }
+}
