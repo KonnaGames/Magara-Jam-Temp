33c156aa497b31cb58e86ae17eaa9367c2c52153
Yunus Emre Sarı <1yesari1@gmail.com>
2023-11-19 02:45:35 +0300

    Added Eye Opening Animation

---
+++ Assets/EyeOpening.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class EyeOpening : MonoBehaviour
+{
+    [SerializeField] private GameObject _top;
+    [SerializeField] private GameObject _bottom;
+
+    private void Awake()
+    {
+        StartEyeOpening();
+    }
+
+    private void StartEyeOpening()
+    {
+        Sequence sequence = DOTween.Sequence();
+        Tweener tweener1 = _top.transform.DOLocalMoveY(-300, 1);
+        Tweener tweener2 = _bottom.transform.DOLocalMoveY(300, 1);
+
+        Tweener tweener3 = _top.transform.DOLocalMoveY(-450, 1.5f);
+        Tweener tweener4 = _bottom.transform.DOLocalMoveY(450, 1.5f);
+
+        Tweener tweener5 = _top.transform.DOLocalMoveY(750, 2);
+        Tweener tweener6 = _bottom.transform.DOLocalMoveY(-750, 2);
+        sequence.Append(tweener1);
+        sequence.Join(tweener2);
+        sequence.Append(tweener3);
+        sequence.Join(tweener4);
+        sequence.Append(tweener5);
+        sequence.Join(tweener6);
+
+        sequence.OnComplete(() => { gameObject.SetActive(false); });
+    }
+}
