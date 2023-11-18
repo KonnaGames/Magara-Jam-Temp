9e09f71fa2e0c26135141f20df79a306556db52b
Kadir Kul <kkul40@gmail.com>
2023-11-19 02:59:21 +0300

    Scene Test

--- Assets/PlayerSpawnManager.cs
+++ Assets/PlayerSpawnManager.cs
@@ -18,35 +18,19 @@ public class PlayerSpawnManager : MonoBehaviour
         DontDestroyOnLoad(this.gameObject);
     }
     
-    private void Start()
-    {
-        Debug.Log("Player Spawn Start Calisti");
-        lastPosInt = PlayerPrefs.GetInt("LastPosInt");
-        Debug.Log(lastPosInt + " +++");
-    }
-
     public Vector3 SetPlayerPositionBySpawnPoints()
     {
-        // if (lastPosInt > SpawnPoints.Count - 1) return SpawnPoints[lastPosInt - 1].position;
-        Debug.Log(SpawnPoints[lastPosInt].position);
         return SpawnPoints[lastPosInt].position;
     }
 
     public void SpawnPointArttir()
     {
-        Debug.Log("Spawn Arttir Test");
-        lastPosInt = PlayerPrefs.GetInt("LastPosInt");
         lastPosInt++;
-        PlayerPrefs.SetInt("LastPosInt", lastPosInt);
-        
-        Debug.Log(lastPosInt + " last Pos");
-        Debug.Log(PlayerPrefs.GetInt("LastPosInt") + " saved one");
     }
 
     [ContextMenu("RestartSpawnPos")]
     public void RestartSpawnPos()
     {
         lastPosInt = 0;
-        PlayerPrefs.SetInt("LastPosInt",lastPosInt);
     }
 }
