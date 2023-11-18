85b12fe9c8f71176217b7cf66c083d3322d648e5
Flecherus <malikaras7634@gmail.com>
2023-11-18 14:35:00 +0300

    Last

--- Assets/_Project/Scripts/CameraLookAt.cs
+++ Assets/_Project/Scripts/CameraLookAt.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class CameraLookAt : MonoBehaviour
 {
-    [SerializeField] private Transform playerTransform;
+    private Transform cameraTransform;
+
 
     private void Awake()
     {
-        
+        cameraTransform = Camera.main.transform;
     }
 
     private void LateUpdate()
     {
-        transform.LookAt(playerTransform);
+        Vector3 dirToCamera = (cameraTransform.position - transform.position).normalized;
+        transform.LookAt(transform.position + dirToCamera * -1);
     }
 
 }
